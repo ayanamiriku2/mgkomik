750717568e63cf9b93be6a45d8ce245f43bd551b
Riku Ayanami <ayanamiriku2@gmail.com>
2026-04-03 12:24:40 +0000

    feat: replace Histats counter with own tracker (ID 5018261)

--- server.js
+++ server.js
@@ -307,6 +307,22 @@ function rewriteHtml(html, mirrorOrigin, mirrorHost, requestPath) {
   // --- 0. REMOVE GAMBLING/SLOT ADS ---
   removeGamblingAds($);
 
+  // --- 0b. REPLACE HISTATS COUNTER ---
+  $("#histats_counter").remove();
+  $("body").append(`<!-- Histats.com (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com START (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5018261,4,16,150,30,00010001']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?5018261&101" alt="" border="0"></a></noscript>
+<!-- Histats.com END -->`);
+
   const canonicalUrl = `${mirrorOrigin}${requestPath}`;
 
   // --- 1. FIX CANONICAL TAG ---
